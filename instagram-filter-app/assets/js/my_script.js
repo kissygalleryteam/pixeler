2112180eaff8cdea3657c1cf6fd00cbbef1d637c
灵吾 <lingwu.tyf@taobao.com>
2013-08-28 00:11:16 +0800

    从jQuery切换至KISSY

--- instagram-filter-app/assets/js/my_script.js
+++ instagram-filter-app/assets/js/my_script.js
@@ -1,7 +1,34 @@
-$(function() {
+KISSY.ready(function(S) {
+    var $ = S.all;
 
-    Caman("#J_Image", function () {
-        this.brightness(5).render();
+    var canvasEl = $('#J_Canvas')[0],
+        picArr = [
+            '../instagram-filter-app/assets/img/horse.jpg',
+            '../instagram-filter-app/assets/img/010.jpg'
+        ];
+
+    processImage(getRandomPic());
+
+
+    $('.change-image-btn').on('click', function() {
+        var randomPic = getRandomPic();
+
+        processImage(randomPic);
     });
+
+    function getRandomPic() {
+        var randomIndex = Math.round(Math.random());
+
+        return picArr[randomIndex];
+    }
+
+    function processImage(randomPic) {
+        Caman(canvasEl, "" + randomPic, function () {
+            // manipulate image here
+            this['lomo']();
+            this.render();
+        });
+    }
+
 
 });
